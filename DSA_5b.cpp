c5c49a06b57a0f6e04c21582004c8e8e3e2e3fc8
ROHIT CHAUDHARY <84496224+rohit-2994@users.noreply.github.com>
2025-04-25 05:06:34 +0530

    Add files via upload

---
+++ DSA_5b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int n;
+    // cin >> n;
+    // bool isPrime = 1;
+    // for (int i = 2; i < n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         isPrime = 0;
+    //         break;
+    //     }
+    // }
+    // if (isPrime == 0)
+    // {
+    //     cout << "The given number is Not Prime";
+    // }
+    // else
+    // {
+
+    //     cout << "The given number is Prime";
+    // }
+
+
+    /*CONTINUE statement permanently removes the below part from execution.*/
+    // for(int i=0; i<5;i++){
+    //     cout<<"hi\n";
+    //     cout<<"hey\n";
+    //     continue;
+    //     cout<<"reply krna.\n";
+    // }
+
+
+    /*GARBAGE VALUE DEMONSTRATION*/
+    // int a;
+    // cout<<a;
+
+
+    // int a=1;
+    // cout<<a<<endl;
+
+    // if(true){
+    //     int a=2; //this a is lying inside these scopes only, not in the main function, hence statement valid.
+    //     cout<<a<<endl;  
+    // } 
+   }
