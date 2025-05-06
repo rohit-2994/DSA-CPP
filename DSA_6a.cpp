b8336d1a1e32740aa68882e52cea2cb8d9ebd41c
ROHIT CHAUDHARY <84496224+rohit-2994@users.noreply.github.com>
2025-05-07 00:45:23 +0530

    Add files via upload

---
+++ DSA_6a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    /*Decimal to Binary*/
+
+    // float ans = 0;
+    // int i = 0;
+    // while (n != 0)
+    // {
+    //     int bit = n & 1;
+    //     ans = (bit * pow(10, i)) + ans;
+    //     n = n >> 1;
+    //     i++;
+    // }
+    // cout << ans;
+
+
+    /*Negative decimal to Binary*/
+    // int a = (~(-1*n)) + 1;
+    // int i=0;
+    // float ans=0;
+    // while(a!=0){
+    //     int bit= a&1;
+    //     ans = (bit+pow(10,i)) + ans;
+    //     i++;
+    //     a>>1;
+    // }
+    // cout<<ans;
+    /*((FINAL SOLUTION PENDING))*/
+
+    /*Binary to Decimal*/
+    // int i=0,ans=0;
+    // while(n!=0){
+    //     int bit = n%10;
+    //     if(bit==1){
+    //         ans = pow(2,i) + ans;
+    //     }
+    //     i++;
+    //     n=n/10;
+    // }   
+    // cout<<ans;
+
+}
